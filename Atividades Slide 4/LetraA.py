645ca4ebe27d98a74bb320f6f2eac3e44f8bf447
terykpy <eryckkayck352@gmail.com>
2026-03-25 13:08:32 -0300

    LetraA.py

---
+++ Atividades Slide 4/LetraA.py
@@ -0,0 +1,10 @@
+#Algoritmo de Multiplicação
+
+print("Digite dois números os quais você deseja multiplicar.")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+"""Área da multiplicação"""
+
+resultado = numero1 * numero2
+print("O resultado da multiplicação entre o número", numero1, "e o número", numero2, "foi", resultado,)
