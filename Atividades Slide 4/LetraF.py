35a82ca06eb6b73cfda6e228b4da2177e53aa838
Eryck <eryckkayck352@gmail.com>
2026-03-30 23:06:03 -0300

    LetraE.py

---
+++ Atividades Slide 4/LetraF.py
@@ -0,0 +1,8 @@
+#Algorítmo de Converter Graus Celsius em Fahrenheit
+
+celsius = float(input("Digite o valor de Graus Celsius: "))
+
+'''Calculo'''
+
+fh = (celsius * 1.8) + 32
+print("O resultado da conversão foi:",fh)
