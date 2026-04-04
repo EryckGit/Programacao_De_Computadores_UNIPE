4dccc12aa3a124fa20afc31fb270c00cbb785d2d
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:48:34 -0300

    Questão5.py
    Add a script to read a number, calculate its triple, and display the type.

---
+++ Atividades Slide 5/Questão5.py
@@ -0,0 +1,7 @@
+#Leia um número como texto (input) e: Converta para inteiro; Mostre o triplo.
+
+numero = input("Digite um número: ")
+numero = int(numero)
+resultado = numero * 3
+print(resultado)
+print(type(numero))
