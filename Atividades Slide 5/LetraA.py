41baa6d7c0f0e4748bbf91f06d04ce9335e99e03
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:17:27 -0300

    LetraA.py

--- Atividades Slide 5/LetraA.py
+++ Atividades Slide 5/LetraA.py
@@ -1,1 +1,9 @@
+#Leia um número inteiro e informe se ele é positivo ou negativo.
 
+print("Informe no número inteiro e o sistema dirá se é positivo ou negativo!")
+número = int(input("Digite o número inteiro: "))
+
+if número >= 0:
+    print("Seu número é positivo")
+else:
+    print("Seu número é negativo")
