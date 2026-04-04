b926b6ad8414d32522d4ca168170fed7eb29c8ce
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:42:36 -0300

    Questão4.py
    Added a script to check if a number is even or odd.

---
+++ Atividades Slide 5/Questão4.py
@@ -0,0 +1,9 @@
+#Leia um número e informe se ele é par ou ímpar.
+
+print("Digite um número e o sistema irá informar se ele é impar ou par!")
+numero = float(input("Digite seu número: "))
+
+if numero % 2 == 0:
+    print("Seu número é par!!")
+else:
+    print("Seu número é impar!!")
