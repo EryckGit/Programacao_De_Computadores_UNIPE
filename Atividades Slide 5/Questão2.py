5f104828dda9c0552c46f7eee1947540689958c6
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:28:31 -0300

    Questão2.py
    Implement number input and display its double.

---
+++ Atividades Slide 5/Questão2.py
@@ -0,0 +1,7 @@
+#Leia um número (entrada padrão do input) e: Converta para inteiro; Exiba o dobro do valor.
+
+numero = input("Digite um número: ")
+numero = int(numero)
+resultado = numero * 2
+print(type(numero))
+print(resultado)
