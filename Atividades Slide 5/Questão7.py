0ce4eaf54a3b663f53a5b81620a880a1abc028d1
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:59:47 -0300

    Questão7.py

---
+++ Atividades Slide 5/Questão7.py
@@ -0,0 +1,9 @@
+#Leia um valor e verifique: Se é maior que 10 → “Maior que 10”; Caso contrário → “Menor ou igual a 10".
+
+print("Digite um valor e o sistema irá verificar se ele é maior, menor ou igual a 10.")
+valor = int(input("Digite aqui seu número: "))
+
+if valor > 10:
+    print("Seu número é Maior que 10!")
+else:
+    print("Seu número é menor ou igual a 10")
